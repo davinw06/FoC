79585255d9952db1d832db441e40b2a99fa81bf8
davinw06 <144931594+davinw06@users.noreply.github.com>
2023-11-19 12:24:01 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,8 +17,8 @@ function toggleImage() {
 
   
 function toggleImage2() {
-   var img1 = "https://www.iconsdb.com/icons/preview/black/switch-on-xxl.png";
-   var img2 = "https://www.iconsdb.com/icons/preview/black/switch-off-xxl.png";
+   var img1 = "https://github.com/davinw06/FoC/blob/main/switch-on-xxl.png";
+   var img2 = "https://github.com/davinw06/FoC/blob/main/switch-off-xxl.png";
   
    
    var imgElement = document.getElementById('toggleImage2');
